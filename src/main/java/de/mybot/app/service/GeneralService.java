d074ead28f6db5f46f0e997ac8184fbd9bc708d3
35569 <eslrra18@outlook.de>
2025-01-29 12:19:49 +0100

    Initial Commit

--- src/main/java/de/mybot/app/service/GeneralService.java
+++ src/main/java/de/mybot/app/service/GeneralService.java
@@ -10,11 +10,26 @@ public class GeneralService {
     Ticket ticket = Ticket.getInstance();
 
     public GeneralService() {
-        issues.add(new Issue(1, "Verbindungsprobleme"));
-        issues.add(new Issue(2, "App-/Softwarefehler"));
-        issues.add(new Issue(3, "Einrichtung/Erstinstallation"));
-        issues.add(new Issue(4, "Ersatzteile/Bestellung"));
-        issues.add(new Issue(5, "Direktes Weiterleiten an Service-Mitarbeiter"));
+        issues.add(new Issue(1, "Verbindungsprobleme",
+                "1. Gerät und Router neustarten.\n" +
+                             "2. Sicherstellen, dass das WLAN-Signal stark genug ist.\n" +
+                             "3. Gerät in den Netzwerkeinstellungen zurücksetzen und erneut verbinden.\n"));
+        issues.add(new Issue(2, "App-/Softwarefehler",
+                   "1. App neustarten und auf Updates prüfen.\n" +
+                                "2. Gerät neustarten und erneut koppeln.\n" +
+                                "3. App neu installieren, falls das Problem weiterhin besteht.\n"));
+        issues.add(new Issue(3, "Einrichtung/Erstinstallation",
+                "1. Anleitung aus der Verpackung befolgen.\n" +
+                             "2. Gerät mit der App verbinden und Kalibrierung durchführen.\n"));
+
+        issues.add(new Issue(4, "Ersatzteile/Bestellung",
+                "Ersatzteile können produktspezifisch bestellt werden.\n" +
+                             "Bitte wählen Sie zuerst Ihr Produkt und das entsprechende Problem.\n" +
+                             "Danach können Sie die passenden Ersatzteile bestellen.\n"));
+
+        issues.add(new Issue(5, "Direktes Weiterleiten an Service-Mitarbeiter",
+                "Sie werden an einen Support-Mitarbeiter weitergeleitet.\n" +
+                             "Bitte halten Sie Ihre Geräte-ID und eine Fehlerbeschreibung bereit.\n"));
     }
 
     public void run() {
