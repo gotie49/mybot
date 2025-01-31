94cd5ec3ca09ac5bb0a3a31e3e8e4ae401714937
gotie49 <gerrit.tieben@gmail.com>
2025-01-31 13:55:15 +0100

    remove comments

--- src/main/java/de/mybot/app/App.java
+++ src/main/java/de/mybot/app/App.java
@@ -6,19 +6,5 @@ public class App {
     public static void main(String[] args) {
         ChatService chatService = new ChatService();
         chatService.startChat();
-        /* TODO: Bug testen, Chatanweisungen überarbeiten/überprüfen, 
-               Vergleich mit Mitbewerben(IKEA-Bot,H&M Chatbot), 
-               Manual/Aktivitätsdiagramm/USE-Case Diagramm besprechen, Fachgespräch planen */
-        /* Aufgabenverteilung: Esli:  
-                               Gerrit: Bug fixen, Programm überprüfen/verbessern
-                               Jonathan: Bug fixen */
-        /* Fachgespräch: 1. Manual 2. Aktivitätsdiagramm, 3. USE-Case Diagramm, 
-        4. Programm zeigen  
-            1. Hauptablauf   
-            2. UIEngine -> gefolgt von PseudoCode
-        5. Ticket zeigen
-            2.
-            
-         */
     }
 }
